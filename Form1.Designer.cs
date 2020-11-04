75285037bc4def6376e4bf9e5d4b83dd9a64dfb3
mohanaveluk <mohanaveluk@gmail.com>
2020-11-04 00:13:00 -0600

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace PDFReader
+{
+    partial class frmPdfReader
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.Button btnSubmit;
+            this.rtbPdfContent = new System.Windows.Forms.RichTextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.txtFileName = new System.Windows.Forms.TextBox();
+            this.btnBlowse = new System.Windows.Forms.Button();
+            btnSubmit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnSubmit
+            // 
+            btnSubmit.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            btnSubmit.Location = new System.Drawing.Point(898, 27);
+            btnSubmit.Name = "btnSubmit";
+            btnSubmit.Size = new System.Drawing.Size(100, 34);
+            btnSubmit.TabIndex = 3;
+            btnSubmit.Text = "Submit";
+            btnSubmit.UseVisualStyleBackColor = true;
+            btnSubmit.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // rtbPdfContent
+            // 
+            this.rtbPdfContent.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rtbPdfContent.Location = new System.Drawing.Point(12, 71);
+            this.rtbPdfContent.Name = "rtbPdfContent";
+            this.rtbPdfContent.Size = new System.Drawing.Size(1099, 607);
+            this.rtbPdfContent.TabIndex = 0;
+            this.rtbPdfContent.Text = "";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // txtFileName
+            // 
+            this.txtFileName.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtFileName.Location = new System.Drawing.Point(12, 31);
+            this.txtFileName.Name = "txtFileName";
+            this.txtFileName.Size = new System.Drawing.Size(767, 26);
+            this.txtFileName.TabIndex = 1;
+            // 
+            // btnBlowse
+            // 
+            this.btnBlowse.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBlowse.Location = new System.Drawing.Point(785, 27);
+            this.btnBlowse.Name = "btnBlowse";
+            this.btnBlowse.Size = new System.Drawing.Size(100, 34);
+            this.btnBlowse.TabIndex = 2;
+            this.btnBlowse.Text = "Browse";
+            this.btnBlowse.UseVisualStyleBackColor = true;
+            this.btnBlowse.Click += new System.EventHandler(this.btnBlowse_Click);
+            // 
+            // frmPdfReader
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1123, 690);
+            this.Controls.Add(btnSubmit);
+            this.Controls.Add(this.btnBlowse);
+            this.Controls.Add(this.txtFileName);
+            this.Controls.Add(this.rtbPdfContent);
+            this.Name = "frmPdfReader";
+            this.Text = "PDF Reader";
+            this.Load += new System.EventHandler(this.frmPdfReader_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox rtbPdfContent;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox txtFileName;
+        private System.Windows.Forms.Button btnBlowse;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using iTextSharp.text.pdf;
+using iTextSharp.text.pdf.parser;
+
+namespace PDFReader
+{
+    public partial class frmPdfReader : Form
+    {
+        public string _fileName = "";
+        public frmPdfReader()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ReadPdfFile(txtFileName.Text);
+        }
+
+        private void btnBlowse_Click(object sender, EventArgs e)
+        {
+            using(OpenFileDialog ofd = new OpenFileDialog() {Filter="PDF files|*.pdf",ValidateNames=true,Multiselect = false })
+            { 
+                if(ofd.ShowDialog() == DialogResult.OK)
+                {
+                    try
+                    {
+                        txtFileName.Text = ofd.FileName;
+                        ReadPdfFile(ofd.FileName);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                        throw;
+                    }
+                }
+            }
+        }
+
+        private void ReadPdfFile(string fileName)
+        {
+            var sb = new StringBuilder();
+            using (PdfReader reader = new PdfReader(fileName))
+            {
+                for (var pgNo = 1; pgNo <= reader.NumberOfPages; pgNo++)
+                {
+                    ITextExtractionStrategy strategy = new SimpleTextExtractionStrategy();
+                    var text = PdfTextExtractor.GetTextFromPage(reader, pgNo, strategy);
+                    text = Encoding.UTF8.GetString(ASCIIEncoding.Convert(Encoding.Default, Encoding.UTF8, Encoding.Default.GetBytes(text)));
+                    sb.Append(text);
+                }
+            }
+            rtbPdfContent.Text = sb.ToString();
+        }
+
+        private void frmPdfReader_Load(object sender, EventArgs e)
+        {
+        }
+    }
+}
